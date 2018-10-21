0cab9118d9520bba94368223c7e90d2189a1a741
yulingtianxia <yulingtianxia@gmail.com>
2018-10-21 20:22:08 +0800

    add ci

---
+++ ClassWrittenInSwiftKit/ClassWrittenInSwiftKit.h
@@ -0,0 +1,19 @@
+//
+//  ClassWrittenInSwiftKit.h
+//  ClassWrittenInSwiftKit
+//
+//  Created by 杨萧玉 on 2018/10/21.
+//  Copyright © 2018 杨萧玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClassWrittenInSwiftKit.
+FOUNDATION_EXPORT double ClassWrittenInSwiftKitVersionNumber;
+
+//! Project version string for ClassWrittenInSwiftKit.
+FOUNDATION_EXPORT const unsigned char ClassWrittenInSwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClassWrittenInSwiftKit/PublicHeader.h>
+#import <ClassWrittenInSwiftKit/ClassWrittenInSwiftKit.h>
+
