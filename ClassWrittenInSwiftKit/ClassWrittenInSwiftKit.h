98423555c2b3c1cdd663b89a8914886232a46f8c
yulingtianxia <yulingtianxia@gmail.com>
2018-10-21 21:05:54 +0800

    fix release build error

--- ClassWrittenInSwiftKit/ClassWrittenInSwiftKit.h
+++ ClassWrittenInSwiftKit/ClassWrittenInSwiftKit.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double ClassWrittenInSwiftKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char ClassWrittenInSwiftKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ClassWrittenInSwiftKit/PublicHeader.h>
-#import <ClassWrittenInSwiftKit/ClassWrittenInSwiftKit.h>
+#import <ClassWrittenInSwiftKit/ClassWrittenInSwift.h>
 
